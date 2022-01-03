b5dfa378dc22e69490eb28ace319cad9c8a5325a
Юлия Медведева <ribka1995@mail.ru>
2022-01-03 16:58:37 +0700

    i win

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Random myRnd = new Random();
+            int[] a = new int[10];
+            Console.WriteLine("Исходный массив:");
+
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = myRnd.Next(-50, 50);
+                Console.Write(a[i] + " ");
+                
+            }
+            Console.WriteLine("\nРезультат:");
+
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 0)              
+                Console.Write(a[i] + " ");
+            }
+            
+            
+        }
+    }
